2ef11c2b8a31a48aea0e9cacf838d37d8715d481
batuhanzorbey <batuhanzorbeyzengin@gmail.com>
2024-03-11 10:24:41 +0300

    swagger router delete

--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"github/batuhanzorbeyzengin/insider-messaging-system/controllers"
-	"github/batuhanzorbeyzengin/insider-messaging-system/routes/swagger"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -12,8 +11,6 @@ func SetupRoutes(app *fiber.App) {
 
 	// Middleware
 	app.Use(logger.New())
-
-	swagger.SwaggerConfig(app)
 
 	api := app.Group("/api/v1")
 
